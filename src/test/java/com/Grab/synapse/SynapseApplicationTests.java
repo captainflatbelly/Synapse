5317ad30b81f05b123a118a925c16b6253a0dd88
Harsh Singh <harshsingh@Harshs-MacBook-Pro.local>
2025-09-06 10:34:30 +0530

    init commit

---
+++ src/test/java/com/Grab/synapse/SynapseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Grab.synapse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynapseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
